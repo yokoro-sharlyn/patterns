54d24ad60a79285c6f891efa4926d947f9374376
Alexander <alexander_topilskii@epam.com>
2017-01-03 02:49:25 +0300

    Added point of entering to show Decorator

---
+++ src/com/yokoro/decorator/DecoratorMain.java
@@ -0,0 +1,12 @@
+package com.yokoro.decorator;
+
+/**
+ * Created by Alexander_Topilskii on 1/3/2017.
+ */
+public class DecoratorMain {
+
+    //Point of entering to show Decorator
+    public void main(){
+
+    }
+}
