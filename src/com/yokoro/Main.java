26ef5b5c212b6bc8375921d5488709bfc94ee34a
Alexander <alexander_topilskii@epam.com>
2017-01-03 03:06:21 +0300

    test

--- src/com/yokoro/Main.java
+++ src/com/yokoro/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
